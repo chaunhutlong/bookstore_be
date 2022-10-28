b27d1732787bef2684b7845248cb8cad2a796b0c
khanhphamK2 <20521453@mg.uit.edu.vn>
2022-10-28 08:34:55 +0700

    crud_discount_part3

--- app/Http/Controllers/Api/DiscountController.php
+++ app/Http/Controllers/Api/DiscountController.php
@@ -6,9 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Models\Discount;
 use Illuminate\Http\Request;
 use App\Http\Resources\DiscountResource;
+use Illuminate\Support\Facades\Validator;
 
 class DiscountController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
     public function index()
     {
         $discount = Discount::all();
@@ -16,5 +23,78 @@ class DiscountController extends Controller
             'discounts' => DiscountResource::collection($discount),
             'message' => 'Retrieved successfully'
         ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make($data, ['name' => 'required|max:50']);
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors(), 'Validation Error']);
+        }
+        $discount = Discount::create($data);
+        return response([
+            'discount' => new DiscountResource($discount),
+            'message' => 'Discount created successfully'
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Discount $discount)
+    {
+        return response([
+            'discount' => new DiscountResource($discount),
+            'message' => 'Retrieved successfully'
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Discount $discount)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:50',
+            'value' => 'required',
+            'start_date' => 'required|after($request->end_date)',
+            'end_date' => 'required|before($request->start_date)',
+            'quantity' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response(['error' => $validator->errors(), 'Validation Error']);
+        }
+        $data = $validator->validated();
+        $discount->update($data);
+        return response([
+            'discount' => new DiscountResource($discount),
+            'message' => 'Updated successfully'
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Discount  $discount
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Discount $discount)
+    {
+        $discount->delete();
+        return response(['message' => 'Deleted']);
     }
 }
