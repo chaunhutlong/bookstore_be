56b724fb0a25d560213c6cbf5823cdfa8d621b1d
LongChau <20520238@gm.uit.edu.vn>
2022-11-04 20:42:10 +0700

    fix swaggers

--- app/Virtual/StorePublisherRequest.php
+++ app/Virtual/StorePublisherRequest.php
@@ -5,7 +5,7 @@
  *      title="Store Publisher request",
  *      description="Store Publisher request body data",
  *      type="object",
- *      required={"name"}
+ *      required={"name, address, phone_number"}
  * )
  */
 
@@ -21,4 +21,33 @@ class StorePublisherRequest
      * @var string
      */
     public $name;
+
+    /**
+     * @OA\Property(
+     *      title="address",
+     *     description="Address of the new publisher",
+     *    example="A nice address"
+     * )
+     * @var string
+     */
+    public $address;
+
+    /**
+     * @OA\Property(
+     *      title="phone_number",
+     *     description="Phone number of the new publisher",
+     *    example="0123456789"
+     * )
+     * @var string
+     */
+    public $phone_number;
+
+    /**
+     * @OA\Property(
+     *      title="description",
+     *     description="Description of the new publisher",
+     *    example="A nice description"
+     * )
+     * @var string
+     */
 }
