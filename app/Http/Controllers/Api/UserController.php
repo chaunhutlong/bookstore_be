e1bd10d928cec1fb05d4e2cb5c6bc97dd54602ce
LongChau <20520238@gm.uit.edu.vn>
2022-11-06 01:00:42 +0700

    upload avatar

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,6 +8,8 @@ use App\Models\UserInfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+
 
 class UserController extends Controller
 {
@@ -15,7 +17,14 @@ class UserController extends Controller
     {
         $user = auth()->user();
         $userInfo = UserInfo::where('user_id', $user->id)->first();
-        return response(['user' => new UserResource($userInfo), 'message' => 'Retrieved successfully'], 200);
+        // return image url from storage
+        $userInfo->avatar = $userInfo->avatar ? asset('storage/' . $userInfo->avatar) : null;
+        $user->userInfo = $userInfo;
+        return response([
+            'success' => true,
+            'data' => new UserResource($user),
+            'message' => 'User profile was successfully retrieved'
+        ]);
     }
 
     public function createOrUpdateProfile(Request $request)
@@ -28,16 +37,39 @@ class UserController extends Controller
                 'address' => 'string|max:15',
                 'phone_number' => 'numeric|digits:10',
                 'bio' => 'string|max:255',
-                'avatar' => 'string',
+                'avatar' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             ]);
 
             $data = $validator->validated();
 
-            // create or update user info
-            $userInfo = UserInfo::updateOrCreate(
-                ['user_id' => $user->id],
-                $data
-            );
+            $userInfo = UserInfo::where('user_id', $user->id)->first();
+
+            // add new avatar to storage and delete old avatar
+            if (
+                $request->hasFile('avatar') && request()->file('avatar')->isValid()
+                && Storage::disk('public')->exists($userInfo->avatar)
+            ) {
+                $avatar = $request->file('avatar');
+                $avatarName = time() . '.' . $avatar->getClientOriginalExtension();
+                $avatarPath = $avatar->storeAs('avatars', $avatarName);
+                $data['avatar'] = $avatarPath;
+                $oldAvatar = $userInfo->avatar;
+                if ($userInfo->avatar) {
+                    Storage::disk('public')->delete($oldAvatar);
+                }
+            }
+
+            if ($userInfo) {
+                $userInfo->update($data);
+            } else {
+                $userInfo = UserInfo::create([
+                    'user_id' => $user->id,
+                    'address' => $data['address'],
+                    'phone_number' => $data['phone_number'],
+                    'bio' => $data['bio'],
+                    'avatar' => $data['avatar'],
+                ]);
+            }
 
             DB::commit();
             return response(['user_info' => new UserResource($userInfo), 'message' => 'User info created successfully']);
