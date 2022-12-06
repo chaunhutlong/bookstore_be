c87ad488eb0be90b1361398bea3f00901d7982ca
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2022-12-06 12:52:43 +0700

    [fix] Order Factory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -16,11 +16,19 @@ class OrderFactory extends Factory
      */
     public function definition()
     {
+        $is_deleted = fake()->boolean();
+        $deleted_at = null;
+        $order_on = null;
+        while ($is_deleted <= $order_on || $order_on != null) {
+
+        }
+        if ($is_deleted)
         return [
-            'status' => fake()->numberBetween(1, 3),
-            'order_on' => fake()->dateTimeThisYear(),
+            'status' => fake()->numberBetween(0, 4),
+            'order_on' =>
             'user_id' => fake()->numberBetween(1, 10),
-            'payment_id' => fake()->numberBetween(1, 5)
+            'payment_id' => fake()->numberBetween(1, 5),
+            'is_deleted' => fake()->boolean()
         ];
     }
 }
