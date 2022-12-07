d189438786fad51f75ba601c2c8e4968dc2122cd
Huỳnh Gia Phú <20521752@gm.uit.edu.vn>
2022-12-07 09:34:27 +0700

    fix CityController

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -1,13 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\City;
+
 class CityController extends Controller
 {
-public static $uitLocation = [
-'lat' => 10.87,
-'lng' => 106.8
-];
-public static function cityDistance(City $city)
-{
-$dX = self::$uitLocation['lat'] - $city->lat;
-$dY = self::$uitLocation['lng'] - $city->lng;
-return 100 * sqrt($dX * $dX + $dY * $dY);
-}
+    public static $uitLocation = [
+        'lat' => 10.87,
+        'lng' => 106.8
+    ];
+
+    public static function cityDistance(City $city) {
+        $dX = self::$uitLocation['lat'] - $city->lat;
+        $dY = self::$uitLocation['lng'] - $city->lng;
+        return 100 * sqrt($dX * $dX + $dY * $dY);
+    }
 }
