506c250c6871657b6b513f36597eae087e6b3db6
khanhphamK2 <20521453@mg.uit.edu.vn>
2022-12-10 21:17:19 +0700

    add BookAuthorTableSeeder

--- app/Models/BookAuthor.php
+++ app/Models/BookAuthor.php
@@ -9,5 +9,7 @@ class BookAuthor extends Pivot
 {
     use HasFactory;
 
-    protected $table = 'book_authors';
+    protected $table = 'books_authors';
+    protected $fillable = ['book_id', 'author_id'];
+    public $timestamps = false;
 }
